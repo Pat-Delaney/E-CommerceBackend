648b45ddaca8441a0517018fb276cf61cd1193c5
Patrick <patrick@Patricks-iMac.local>
2023-02-12 13:02:10 -0800

    implemented route calls

--- models/index.js
+++ models/index.js
@@ -5,13 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
 
+  through: ProductTag
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
 
+  through: ProductTag
+})
+Product.hasMany(ProductTag,{
+  foreignKey: 'product_id',
+});
+ProductTag.belongsTo(Product,{
+  foreignKey: 'product_id',
+});
+Tag.hasMany(ProductTag,{
+  foreignKey: 'tag_id',
+});
+ProductTag.belongsTo(Tag,{
+  foreignKey: 'tag_id',
+});
 module.exports = {
   Product,
   Category,
